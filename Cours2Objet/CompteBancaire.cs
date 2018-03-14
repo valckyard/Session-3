ed4f96d18aa7e3468554d8e99e75dfd2a2a5178b
Alexandre Caron <alexcaron880@gmail.com>
2018-03-14 09:45:04 -0400

    changes

--- Cours2Objet/CompteBancaire.cs
+++ Cours2Objet/CompteBancaire.cs
@@ -8,5 +8,26 @@ namespace Cours2Objet
 {
     class CompteBancaire
     {
+        public string Titulaire { get; set; }
+        public double Solde { get; set; }
+        public string Devise { get; set; }
+
+        public CompteBancaire(string titulaire, double solde, string devise)
+        {
+            Titulaire = titulaire;
+            Solde = solde;
+            Devise = devise;
+        }
+
+        public void Crediter(double montant)
+        {
+            Solde += montant;
+        }
+
+        public void Debiter(double montant)
+        {
+            Solde -= montant;
+        }
+
     }
 }
