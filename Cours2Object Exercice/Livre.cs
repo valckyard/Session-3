420b19074242b43d88beddecb81af4ef9ffdd7ad
Alexandre Caron <alexcaron880@gmail.com>
2018-03-14 10:54:32 -0400

    yea

---
+++ Cours2Object Exercice/Livre.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cours2Object_Exercice
+{
+    public class Livre
+    {
+        public string Titre { get; set; }
+        public string NomAuteur { get; set; }
+        public double Prix { get; set; }
+
+        public Livre(string titre, string nomAuteur, double prix)
+        {
+            Titre = titre;
+            NomAuteur = nomAuteur;
+            Prix = prix;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Titre  : "+ Titre +"\n" +
+                              "Auteur : " + NomAuteur+ "\n" +
+                              "Prix   : "+ Prix +"\n");
+
+        }
+    }
+}
---
+++ Cours2Object Exercice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours2Object_Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sda = new Livre("Le Seigneur des Anneaux Book 1","J.R.R. Tolkien", 39.99);
+            var sda2 = new Livre("Le Seigneur des Anneaux Book 2", "J.R.R. Tolkien", 29.99);
+
+            sda.Afficher();
+            sda2.Afficher();
+        }
+    }
+}
