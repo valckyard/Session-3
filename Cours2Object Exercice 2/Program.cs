e936ce785860461970e034feb1477c76e9b9ff96
Alexandre Caron <alexcaron880@gmail.com>
2018-03-14 11:52:32 -0400

    rectangle

---
+++ Cours2Object Exercice 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours2Object_Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monrect = new Rectangle(5, 12);
+
+            monrect.AfficherRectangle();
+            Console.ReadLine();
+            Console.Clear();
+
+            var monrect1 = new Rectangle(5, 5);
+
+            monrect1.AfficherRectangle();
+            Console.ReadLine();
+            Console.Clear();
+
+            var monrect2 = new Rectangle(12, 2);
+
+            monrect2.AfficherRectangle();
+            Console.ReadLine();
+            Console.Clear();
+
+            var monrect3 = new Rectangle(8, 8);
+
+            monrect3.AfficherRectangle();
+            Console.ReadLine();
+            Console.Clear();
+
+            var monrect4 = new Rectangle(2, 2);
+
+            monrect4.AfficherRectangle();
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ Cours2Object Exercice 2/Rectangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours2Object_Exercice_2
+{
+    public class Rectangle
+    {
+        public int Longeur { get; set; }
+        public int Largeur { get; set; }
+
+        public Rectangle(int longeur, int largeur)
+        {
+            Longeur = longeur;
+            Largeur = largeur;
+        }
+
+        public int Perimetre()
+        {
+            int perimetre = (Longeur * 2) + (Largeur * 2);
+            return perimetre;
+        }
+
+        public int Aire()
+        {
+            int aire = Longeur * Largeur;
+            return aire;
+        }
+
+        public string EstCarre()
+        {
+            if (Longeur == Largeur)
+            {
+                return "Est un Carre !";
+            }
+            else
+            {
+                return "N'est pas un Carre !";
+            }
+        }
+
+        public void AfficherRectangle()
+        {
+            Console.Write("╔");
+            for (int i = 0; i < Largeur*2; i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╗");
+
+
+            int counter = 1;
+            for (int i = 0; i < Longeur; i++)
+            {
+                Console.SetCursorPosition(0,counter);
+                Console.Write("║");
+                Console.SetCursorPosition(Largeur*2+1,counter);
+                Console.Write("║\n");
+                ++counter;
+            }
+
+            Console.SetCursorPosition(0,counter);
+            Console.Write("╚");
+            for (int i = 0; i < Largeur*2; i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╝");
+
+            ++counter;
+            Console.SetCursorPosition(0,counter);
+            Console.Write($"Longeur   : {Longeur}\n" +
+                          $"Largeur   : {Largeur}\n" +
+                          $"Aire      : {Aire()}\n" +
+                          $"Perimetre : {Perimetre()}\n" +
+                          $"Carre     : {EstCarre()}\n");
+
+        }
+    }
+}
