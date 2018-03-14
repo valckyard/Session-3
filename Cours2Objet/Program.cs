b53af1c60c1ea23af8f37374d84bd4d242a70b8e
Alexandre Caron <alexcaron880@gmail.com>
2018-03-14 09:28:18 -0400

    cours 2

---
+++ Cours2Objet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours2Objet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
