f22b2c05691edd1ca45ef4cca97ae3292049292e
Alexandre Caron <alexcaron880@gmail.com>
2018-03-23 12:33:24 -0400

    Cours 6 init

---
+++ Cours6Object Exercice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours6Object_Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
