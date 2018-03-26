c9fe461f82f4236d932d4dbb657f813fbd939308
Alexandre Caron <alexcaron880@gmail.com>
2018-03-26 15:44:19 -0400

    WHAT WHAT WHAT

--- Cours7Object Exercice2/Program.cs
+++ Cours7Object Exercice2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LibS3.C7;
 
 namespace Cours7Object_Exercice2
@@ -14,7 +10,7 @@ namespace Cours7Object_Exercice2
 
             var comptestd = new CompteC7(122);
 
-            var compteep = new CompteEpargneEX2C7(444);
+            var compteep = new CompteEpargneC7(444);
 
             var comptepay = new ComptePayantC7(100);
 
